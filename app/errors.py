7891432d46edace6f1dcb92c0c161060ae57bbae
Sashaaaaaa6 <o.bulavin14@gmail.com>
2025-12-04 19:32:29 +0200

    Update errors.py

--- app/errors.py
+++ app/errors.py
@@ -1,1 +1,14 @@
+class VaccineError(Exception):
+    """Base vaccine exception."""
 
+
+class NotVaccinatedError(VaccineError):
+    """Raised when a visitor has no vaccine."""
+
+
+class OutdatedVaccineError(VaccineError):
+    """Raised when vaccine is expired."""
+
+
+class NotWearingMaskError(Exception):
+    """Raised when visitor is not wearing a mask."""
